318571884491973a4855b461323498197e544d6b
Christopher R. Krenn <crkrenn@rztopaz578.llnl.gov>
2021-01-20 18:31:17 -0800

    finished updates to parameter_block documentation

--- scisample/base_sampler.py
+++ scisample/base_sampler.py
@@ -198,25 +198,25 @@ class BaseSampler(SamplerInterface):
         
         Given the following sampler:
         
-        |type: list
-        |constants:
-        |    X1: 20
-        |parameters:
-        |    X2: [5, 10]
-        |    X3: [5, 10]
-        |    X4: [5, 10]
+        | type: list
+        | constants:
+        |     X1: 20
+        | parameters:
+        |     X2: [5, 10]
+        |     X3: [5, 10]
+        |     X4: [5, 10]
 
         ``get_samples`` returns a list of dictionaries:
 
-        |[{'X1': 20, 'X2': 5, 'X3': 5, 'X4': 5}, 
-        | {'X1': 20, 'X2': 10, 'X3': 10, 'X4': 10}]
+        | [{'X1': 20, 'X2': 5, 'X3': 5, 'X4': 5}, 
+        |  {'X1': 20, 'X2': 10, 'X3': 10, 'X4': 10}]
 
         and ``parameter_block`` returns a dictionary of dictionaries:
 
-        |{'X1': {'values': [20, 20], 'label': 'X1.%%'}, 
-        | 'X2': {'values': [5, 10], 'label': 'X2.%%'}, 
-        | 'X3': {'values': [5, 10], 'label': 'X3.%%'}, 
-        | 'X4': {'values': [5, 10], 'label': 'X4.%%'}}
+        | {'X1': {'values': [20, 20], 'label': 'X1.%%'}, 
+        |  'X2': {'values': [5, 10], 'label': 'X2.%%'}, 
+        |  'X3': {'values': [5, 10], 'label': 'X3.%%'}, 
+        |  'X4': {'values': [5, 10], 'label': 'X4.%%'}}
         """ # noqa
         if self._parameter_block is None:
             self._parameter_block = {}
